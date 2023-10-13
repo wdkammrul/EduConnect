024cf1745f40fcea23ed68b4e654a81e2eb6f208
Md Kamrul Islam <kamruldca@gmail.com>
2023-10-13 23:39:34 +0600

    Responsive all ok

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -2,7 +2,7 @@
 
 const AboutUs = () => {
     return (
-        <div className="my-16 p-16 rounded-lg text-center bg-slate-800 text-white">
+        <div className="my-16 p-16 rounded-lg text-center bg-slate-800 text-white  w-[400px] md:w-[740px] lg:w-full mx-auto">
             <h2 className="text-5xl text-primary mb-8 font-bold">About Us</h2>
             <p className="text-xl px-3">Welcome to EduConnect go-to destination for educational and training events! We curate workshops, webinars, career fairs, conferences, and training programs to fuel your learning journey. Our mission is to empower individuals, connecting them with valuable opportunities and fostering continuous growth. Join us, explore new possibilities, and enhance your skills on the path to success. Connect, learn, and thrive with EduConnect. 
             </p>
--- src/pages/Home/ContactUs.jsx
+++ src/pages/Home/ContactUs.jsx
@@ -2,10 +2,10 @@
 
 const ContactUs = () => {
     return (
-        <div className="hero bg-base-200 rounded-lg p-6">
+        <div className="hero  w-[400px] md:w-[740px] lg:w-full mx-auto bg-base-200 rounded-lg p-10">
             <div className="hero-content flex-col ">
                 <div className="text-center lg:text-left">
-                    <h1 className="text-5xl font-bold text-center">Contact Us</h1>
+                    <h1 className="text-5xl font-bold text-center my-6">Contact Us</h1>
                 
                 </div>
                 <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
